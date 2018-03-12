c81a68d25956be499b55c13b3c7cee945d1a2edd
恐高的鸟 <36455147+cnsyear@users.noreply.github.com>
2018-03-12 10:01:05 +0800

    Update Const.java

--- wechat-share/src/main/java/cn/com/highset/common/constant/Const.java
+++ wechat-share/src/main/java/cn/com/highset/common/constant/Const.java
@@ -4,8 +4,8 @@ package cn.com.highset.common.constant;
  * 常量值
  */
 public class Const {
-    public static final String APPID = "wxc857987be4f6d9b2";
-    public static final String AppSecret = "6b1960835b43e0dd24d35ad0a49a25fb";
-    //public static final String MCH_ID = "1341243401";
+    public static final String APPID = "wxc8512321d9b212312";
+    public static final String AppSecret = "6b196083512321312d35ad0a49a25fb";
+    //public static final String MCH_ID = "13411321401";
     public static final String API_KEY = "";
 }
