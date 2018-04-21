043a8a3bcbf3c018749381f6f63d8010a4661357
cnsyear <820382480@qq.com>
2018-04-21 15:54:11 +0800

    2018年4月21日15:54:05

---
+++ 微信小程序/highLight/pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '<高亮 A>',
+    list:[
+      'WaC38',
+      'W2A81',
+      'E2A18',
+      'E2A61',
+      'E3Ca1',
+      'E2C81',
+      'E4a43',
+      'E4A65'
+    ]
+  },
+  onLoad: function () {
+  }
+})
